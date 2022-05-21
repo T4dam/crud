d64bf500633d276dd3553748faca1b71b2308534
TADAS DELININKAITIS <t4dam94@gmail.com>
2022-05-21 23:20:48 +0300

    contact detail page start

--- src/components/contact-card.js
+++ src/components/contact-card.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Pic from './images/acc.png';
+import {Link} from 'react-router-dom'
 
 const ContactCard = (props) => {
 const {id, name, email} = props.contacts
@@ -10,8 +11,10 @@ const {id, name, email} = props.contacts
         <div className="w-full h-auto">
             <div key={id} className='flex justify-between items-center'>
                 <div>
-                    <div className='font-bold'>{name}</div>
-                    <div>{email}</div>
+                    <Link to={`/contact/${id}`}>
+                      <div className='font-bold'>{name}</div>
+                      <div>{email}</div>
+                    </Link>
                 </div>
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" onClick={()=>{props.clickHandler(id)}} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
               <path strokeLinecap="round" strokeLinejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
